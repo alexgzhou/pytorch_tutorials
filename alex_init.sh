702b2d4d965861776811e13c710a9958f729b4f5
alexgzhou <alexgzhou@163.com>
2020-07-06 13:14:42 +0000

    notes in alex_init.sh

--- alex_init.sh
+++ alex_init.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# *.ipynb, *.py 文件在项目中已经存在, 不需要生成;
+# `$ make docs` or `$ make html-noplot` 是生成 html 文档的过程, 不需要执行
+# alex_tools.sh 可以分离出 *.ipynb, *.py 文件到单独的路径下, 方便调试代码 (需要先 `$ chmod 755 alex_tools.sh && apt install cpio -y`, 再 `$ ./alex_tools.sh cpp` or `$ ./alex_tools.sh cpi`)
+
 # 设置原始仓库为 remote.official (upstream 仓库)
 git remote add official https://github.com/pytorch/tutorials.git
 # git config -l | findstr official
--- alex_tools.sh
+++ alex_tools.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [ "$1" == "cp" ]; then
+if [ "$1" == "cpi" ]; then
     cd $DIR && mkdir -p ../temp
     find . -name "*.ipynb" | cpio -updm ../temp 
+fi
+
+if [ "$1" == "cpp" ]; then
+    cd $DIR && mkdir -p ../temp_py
+    find . -name "*.py" | cpio -updm ../temp_py
 fi
